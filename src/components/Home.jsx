d2834faef6d1b8a2f7405b5ebc44dd0faf311462
juan-abraham-dv <123184630+juan-abraham-dv@users.noreply.github.com>
2023-03-15 14:39:17 -0300

    Home done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,77 @@
 import React from "react";
 import LeftPartial from "./LeftPartial";
 import RightPartial from "./RightPartial";
-//import axios from "axios";
+import axios from "axios";
+import "./Home.css";
+import fakerprofile from "./img/fakeprofile.jpg";
 
 function Home() {
-  /* //const [allTweets, setAllTweets] = React.useState([]);
+  const [allTweets, setAllTweets] = React.useState([]);
   React.useEffect(() => {
     const getTweets = async () => {
       const response = await axios.get(`http://localhost:8000/tweets`);
       console.log(response.data.tweets);
-      // setAllTweets(response.data.tweets);
+      setAllTweets(response.data.tweets);
     };
     getTweets();
-  }, []);*/
+  }, []);
 
   return (
     <div className="container">
       <div className="row gx-5">
         <LeftPartial />
-        <div className="col-5"></div>
+        <div className="col-5 p-0">
+          <div className="send-tweet-box p-3">
+            <h1 className="send-tweet-box-title">Home</h1>
+            <div className="col-2 d-inline-block mt-2 mb-2">
+              <img
+                alt="Cualquier cosa"
+                src={fakerprofile}
+                className="img-profile"
+              />
+            </div>
+            <div className="col-9 d-inline-block">
+              <input
+                type="text"
+                placeholder="What's happening?"
+                className="new-tweet-imput"
+              />
+            </div>
+            <button className="d-block send-tweet-btn btn btn-home text-white rounded-pill w-22">
+              Tweet
+            </button>
+          </div>
+          <div>
+            {allTweets.map((tweet) => (
+              <div className="m-3">
+                <div className="row all-tweets-box">
+                  <div className="col-1 all-tweets-img-box">
+                    <img
+                      alt="Cualquier cosa"
+                      src={fakerprofile}
+                      className="img-profile-tweet"
+                    />
+                  </div>
+                  <div className="col-10 mb-3">
+                    <small>
+                      {tweet.author}
+                      {tweet.createdAt}
+                    </small>
+                    <p> {tweet.content}</p>
+                    <small>
+                      <i class="bi bi-heart-fill unliked"></i>
+                      <small className="ms-1 unliked">
+                        {tweet.likes.length}
+                      </small>
+                      <i class="bi bi-heart-fill liked"></i>
+                      <small className="ms-1 liked">{tweet.likes.length}</small>
+                    </small>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
         <RightPartial />
       </div>
     </div>
@@ -26,9 +79,3 @@ function Home() {
 }
 
 export default Home;
-
-/*<div>
-      {allTweets.map((tweet) => (
-        <div className="col">{tweet.content}</div>
-      ))}
-    </div>*/
