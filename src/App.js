79d831e9381bfce92784eeee1d6ce53f4addec61
Oco <nachodecores@hotmail.com>
2023-03-15 11:14:33 -0300

    followers component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 //import store from "./storeConfig";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
+// import Profiles from "./components/Profiles";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="signup" element={<SignUp />} />
+        {/* <Route path="profile" element={<Profiles />} /> */}
         {/* <Route path="login" element={<Home />}/> */}
       </Routes>
     </div>
